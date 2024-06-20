a14f21f25dcc93733a7496af61eb090e0dd07970
naki1993 <168444448+naki1993@users.noreply.github.com>
2024-06-20 22:45:14 +0900

    Add files via upload

---
+++ kadai_mdn_js/mdn.js
@@ -0,0 +1,12 @@
+
+var currentDate = new Date();
+var year = currentDate.getFullYear();
+
+var currentDate = new Date();
+var month = currentDate.getMonth() + 1;
+
+var currentDate = new Date();
+var date = currentDate.getDate();
+
+console.log(year + '年' + month + '月' + date + '日');
+
